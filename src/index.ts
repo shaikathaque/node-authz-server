797bb6ad7f3efa180e4cdd46cb348d42049b10fa
Shaikat Haque <shaikat@Shaikats-MacBook-Pro.local>
2024-11-04 10:36:49 +0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const message: string = "TypeScript setup is working!";
+console.log(message);
+
+// Test that TypeScript compilation is working with a simple function
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(add(5, 4));
