5d87832e7dfd6928f0a315a87b8f837022bccda5
Shaikat Haque <shaikathaque4@gmail.com>
2024-11-04 10:59:50 +0600

    add eslint prettier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port = 3000;
@@ -7,10 +7,13 @@ const port = 3000;
 app.use(express.json());
 
 // Health check route
-app.get('/health', (req: Request, res: Response) => {
-    res.status(200).json({ status: 'ok' });
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok" });
 });
 
+const test = "hi";
+const a = 1;
+
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
